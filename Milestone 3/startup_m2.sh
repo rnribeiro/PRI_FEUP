512d876c4d8654cfffe1cb08d26e6b63310c630c
Ricardo Ribeiro <71141428+rnribeiro@users.noreply.github.com>
2023-12-12 10:54:28 +0000

    updating commands

--- Milestone 3/startup_m2.sh
+++ Milestone 3/startup_m2.sh
@@ -1,11 +1,11 @@
-REM Step 1: Run Solr Docker container
-docker run -p 8983:8983 --name solr -v %cd%:/data -d solr:9.3
+# Step 1: Run Solr Docker container
+docker run -p 8983:8983 --name solr -v "$(pwd)":/data -d solr:9.3
 
-REM Step 2: Create Solr core
+# Step 2: Create Solr core
 docker exec solr bin/solr create_core -c books_m2
 
-REM Step 4: Update Solr schema
+# Step 4: Update Solr schema
 curl -X POST -H "Content-type:application/json" --data-binary "@schema_m2.json" http://localhost:8983/solr/books_m2/schema
 
-REM Step 5: Post data to Solr core
+# Step 5: Post data to Solr core
 docker exec solr bin/post -c books_m2 ../../data/books_data_reviews.json
---
+++ Milestone 3/startup_m3_synonyms.sh
@@ -0,0 +1,14 @@
+# Step 1: Run Solr Docker container
+docker run -p 8983:8983 --name solr -v "$(pwd)":/data -d solr:9.3
+
+# Step 2: Create Solr core
+docker exec solr bin/solr create_core -c books_m3_synonyms
+
+# Step 3: Copy synonyms.txt to Solr container
+docker cp synonyms.txt solr:/var/solr/data/books_m3_synonyms/conf/synonyms.txt
+
+# Step 4: Update Solr schema
+curl -X POST -H "Content-type:application/json" --data-binary "@schema_m3_synonyms.json" http://localhost:8983/solr/books_m3_synonyms/schema
+
+# Step 5: Post data to Solr core
+docker exec solr bin/post -c books_m3_synonyms ../../data/books_data_reviews.json

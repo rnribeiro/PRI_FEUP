d1b6209895d99bbd79eb2999662059eb60482665
Ricardo Ribeiro <71141428+rnribeiro@users.noreply.github.com>
2023-12-13 09:17:51 +0000

    updating embeddings folder

--- Milestone 3/embeddings/startup_m3_embeddings.sh
+++ Milestone 3/embeddings/startup_m3_embeddings.sh
@@ -2,13 +2,10 @@
 docker run -p 8983:8983 --name solr -v "$(pwd)":/data -d solr:9.3
 
 # Step 2: Create Solr core
-docker exec solr bin/solr create_core -c books_m3_synonyms
+docker exec solr bin/solr create_core -c books_m3_embeddings
 
-# Step 3: Copy synonyms.txt to Solr container
-docker cp synonyms.txt solr:/var/solr/data/books_m3_synonyms/conf/synonyms.txt
+# Step 3: Update Solr schema
+curl -X POST -H "Content-type:application/json" --data-binary "@schema_m3_embeddings.json" http://localhost:8983/solr/books_m3_embeddings/schema
 
-# Step 4: Update Solr schema
-curl -X POST -H "Content-type:application/json" --data-binary "@schema_m3_synonyms.json" http://localhost:8983/solr/books_m3_synonyms/schema
-
-# Step 5: Post data to Solr core
-docker exec solr bin/post -c books_m3_synonyms ../../data/books_data_reviews.json
+# Step 4: Post data to Solr core
+docker exec solr bin/post -c books_m3_embeddings ../../data/books_data_reviews_sample_embeddings.json

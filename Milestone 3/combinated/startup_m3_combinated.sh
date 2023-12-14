40315b6fff85a7d93e10210574773e6184ec382f
Ricardo Ribeiro <71141428+rnribeiro@users.noreply.github.com>
2023-12-14 11:12:54 +0000

    updating queries.json and minor fixes

--- Milestone 3/combinated/startup_m3_combinated.sh
+++ Milestone 3/combinated/startup_m3_combinated.sh
@@ -2,13 +2,12 @@
 # docker run -p 8983:8983 --name solr -v "$(pwd)":/data -d solr:9.3
 
 # Step 2: Create Solr core
-docker exec solr bin/solr create_core -c books_m3_conbinated
+docker exec solr bin/solr create_core -c books_m3_combinated
 
 # Step 3: Copy synonyms.txt to Solr container
-docker cp synonyms.txt solr:/var/solr/data/books_m3_conbinated/conf/synonyms.txt
+docker cp synonyms.txt solr:/var/solr/data/books_m3_combinated/conf/synonyms.txt
 
 # Step 4: Update Solr schema
-curl -X POST -H "Content-type:application/json" --data-binary "@schema_m3_synonyms.json" http://localhost:8983/solr/books_m3_conbinated/schema
 
 # Step 5: Post data to Solr core
-docker exec solr bin/post -c books_m3_conbinated ../../data/books_data_reviews.json
+docker exec solr bin/post -c books_m3_combinated ../../data/books_data_reviews.json
